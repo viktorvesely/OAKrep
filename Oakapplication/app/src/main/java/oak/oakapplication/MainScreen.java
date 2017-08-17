7062aee258c9f5a0a6173fa89b8bb51d8d78e81b
rohalmatus <rohalmatus1@gmail.com>
2017-08-17 17:16:59 +0200

    Add files via upload

--- Oakapplication/app/src/main/java/oak/oakapplication/MainScreen.java
+++ Oakapplication/app/src/main/java/oak/oakapplication/MainScreen.java
@@ -5,9 +5,13 @@ import android.os.Bundle;
 import com.facebook.FacebookSdk;
 import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.Button;
@@ -28,7 +32,8 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Objects;
 
-public class MainScreen extends AppCompatActivity {
+public class MainScreen extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener{
 
     private DatabaseReference mRootRef;
     private DatabaseReference mPostRef;
@@ -203,6 +208,31 @@ public class MainScreen extends AppCompatActivity {
             mPostRef.removeEventListener(mPostListener);
             mPostListener = null;
         }
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_camera) {
+            // Handle the camera action
+        } else if (id == R.id.nav_gallery) {
+
+        } else if (id == R.id.nav_slideshow) {
+
+        } else if (id == R.id.nav_manage) {
+
+        } else if (id == R.id.nav_share) {
+
+        } else if (id == R.id.nav_send) {
+
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
     }
 
 
